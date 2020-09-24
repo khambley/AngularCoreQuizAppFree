7b55e9daa5a0b7669578734ccf70cb31b89a2c33
Katherine Hambley <khambley@franklinenergy.com>
2020-09-24 15:51:43 -0500

    Fixed Navigation properties circular ref problem

--- ServerApp/Controllers/QuestionValuesController.cs
+++ ServerApp/Controllers/QuestionValuesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ServerApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ServerApp.Controllers {
     [Route("api/questions")]
@@ -12,9 +14,27 @@ namespace ServerApp.Controllers {
         }
 
         [HttpGet("{id}")]
-        public Question GetQuestion(int id){
-            System.Threading.Thread.Sleep(5000);
-            return context.Questions.Find(id);
-        }
+        public Question GetQuestion(int id) {
+            //System.Threading.Thread.Sleep(5000);
+            Question result = context.Questions
+                .Include(q => q.Quiz).ThenInclude(qu => qu.Questions)
+                .FirstOrDefault(q => q.QuestionId == id);
+            
+            if (result != null) {
+                if (result.Quiz != null) {
+                    result.Quiz.Questions = result.Quiz.Questions.Select(qu => 
+                        new Question {
+                            QuestionId = qu.QuestionId,
+                            Text = qu.Text,
+                            CorrectAnswer = qu.CorrectAnswer,
+                            WrongAnswer1 = qu.WrongAnswer1,
+                            WrongAnswer2 = qu.WrongAnswer2,
+                            WrongAnswer3 = qu.WrongAnswer3
+                        });
+                }
+
+            }
+            return result;
+        }   
     }
 }
